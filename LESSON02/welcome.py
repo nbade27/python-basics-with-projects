990bc45b191e944f2dab0b7af1beb55c27f9e9a5
Naga Sai <fullstacknaga@gmail.com>
2024-03-02 23:40:45 +0530

    welcome!!!!

---
+++ LESSON02/welcome.py
@@ -0,0 +1,23 @@
+# this is the comment in python
+
+
+# valid variables declaration in Python
+
+'''
+name = 'naga' -- valid variable
+_name = 'naga' -- valid variable
+name! = 'naga' -- invalid variable declaration in Python
+name-first = 'naga' -- invalid syntax (python doesnot supports - in variable names)
+0001name = 'naga' -- invalid syntax (python doesnot supports variables that start with numbers)
+'''
+
+line01 = "*****************************"
+line02 = "*                           *"
+line03 = "*         welcome           *"
+
+
+print (line01)
+print (line02)
+print (line03)
+print (line02)
+print (line01)
